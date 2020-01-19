6ac3ebf69fa5ed9c484fd2aafe11f4cbbe966d51
Venkat Santos <nagularaj6119@gmail.com>
2020-01-19 10:49:50 +0530

    download exceldata from mongo to local

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'downloadexcel';
+  constructor(private http: HttpClient) {}
+  title = "downloadexcel";
+  firstVar = "";
+  secondVar = "";
+  thirdVar = "";
+
+  SaveValue = () => {
+    var obj = {
+      firstVar: this.firstVar,
+      secondVar: this.secondVar,
+      thirdVar: this.thirdVar
+    };
+    console.log('comes');
+    this.http
+      .post("http://localhost:3000/saveVlaue", obj)
+      .subscribe(data => {  
+        console.log('success'+data);
+      },
+      error=>{
+        console.log(error);
+      });
+    console.log(obj);
+  };
+
+ DownloadData=()=>{
+   alert('works');
+   this.http
+      .get("http://localhost:3000/downloadValue")
+      .subscribe(data => {  
+        console.log('success'+data);
+      },
+      error=>{
+        console.log(error);
+      });
+ }
+
+
 }
---
+++ src/server.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const body = require("body-parser");
+const cros = require("cors");
+const excel = require('exceljs');
+const varModel = mongoose.model("collection", {
+  firstVar: String,
+  secondVar: String,
+  thirdVar: String
+});
+
+app.use(body.json());
+mongoose.connect("mongodb://localhost:27017/saveExcel", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+var crosobj = {
+  orign: "http://localhost:4200",   
+  optionSucessStatus: 200
+};
+app.use(cros(crosobj));
+
+app.listen(3000, () => {
+  console.log("server listening the port 3000");
+});
+
+app.post("/saveVlaue", (req, res) => {
+  var collectionObj = new varModel();
+  collectionObj.firstVar=req.body.firstVar;
+  collectionObj.secondVar=req.body.secondVar;
+  collectionObj.thirdVar=req.body.thirdVar;
+  collectionObj.save((err,resSave)=>{
+      if(err){
+          console.log(err);
+      }
+      else{
+          console.log(resSave._id);
+          res.send(collectionObj)
+      }
+  })
+});
+
+
+app.get("/downloadValue", (req, res) => {
+    console.log('comes');
+  varModel.find({},{},(err,docs)=>{
+   if(err){
+       console.log(err);
+   }
+   else{
+       console.log(docs);
+       
+  let workbook = new excel.Workbook(); //creating workbook
+  let worksheet = workbook.addWorksheet('document'); 
+   worksheet.columns = [
+    { header: 'Id', key: '_id', width: 10 },
+    { header: 'firstVar', key: 'firstVar', width: 30 },
+    { header: 'secondVar', key: 'secondVar', width: 30},
+    { header: 'thirdVar', key: 'thirdVar', width: 30,}
+  ];
+  worksheet.addRows(docs);
+  workbook.xlsx.writeFile("document.xlsx")
+    .then(function() {
+      console.log("file saved!");
+    });
+   }
+  })
+
+});
